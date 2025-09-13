db8b8431600f50aebb43dbd8d8e3940bef143882
Rodney Gauna <rodneygauna@gmail.com>
2025-09-13 14:02:01 -0700

    refactor: Enhance home page with donor carousel and upcoming event display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   allow_unauthenticated_access
 
   def index
+    # Get public donors for the carousel
+    @business_donors = Donor.businesses.public_donors.limit(6)
+    @individual_donors = Donor.individuals.public_donors.limit(6)
+
+    # Get the next upcoming event
+    @next_event = Event.published.upcoming.order(:starts_at).first
   end
 end
